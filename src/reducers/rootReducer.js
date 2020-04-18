4621e5ea2e6411233e48a6406333bae9c655e30c
bardamuffin <bardamuffin@gmail.com>
2020-04-18 11:38:19 +0200

    passed the test

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,37 +1,19 @@
 const initState = {
   display: '0',
   lastEntered: '',
-  pastCalculation: [
-    '3',
-    '2',
-    '+',
-    '6',
-    '5',
-    '*',
-    '7',
-    '8',
-    '/',
-    '9',
-    '5',
-    '-',
-    '2',
-    '1',
-    '+',
-  ],
+  pastCalculation: ['1', '0', '.', '5', '-', '5', '.', '5'],
 };
 const nbr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const operator = ['-', '+', '*', '/'];
 const rootReducer = (state = initState, action) => {
   //A number has been pressed
   if (action.type === 'DISPLAY_NUMBER') {
-    console.log('A number has been pressed: ', action.msg);
     //if the first number is zero and not dec replace by new value
     if (!isDec(state.display) && state.display[0] === '0') {
       //If two 0 or more followed
       if (action.msg === '0') {
         return state;
       }
-      console.log('res', [...state.lastEntered, ...action.msg]);
       return {
         ...state,
         pastCalculation: [...state.pastCalculation, ...action.msg],
@@ -49,10 +31,8 @@ const rootReducer = (state = initState, action) => {
   }
   //An operator has been pressed
   if (action.type === 'DISPLAY_OPERATOR') {
-    console.log('An operator has been pressed');
     //if the last entered is an operator change for the new
     if (operator.indexOf(state.lastEntered) >= 0) {
-      console.log('last one was an operator');
       return {
         ...state,
         lastEntered: action.msg,
@@ -63,8 +43,6 @@ const rootReducer = (state = initState, action) => {
         ],
       };
     } else {
-      console.log('last one was NOT operator');
-
       return {
         ...state,
         lastEntered: action.msg,
@@ -75,7 +53,6 @@ const rootReducer = (state = initState, action) => {
   }
   //A decimal has been pressed
   if (action.type === 'DISPLAY_DECIMAL') {
-    console.log('A decimal has been pressed');
     //Nbr is already dec, return
     if (isDec(state.display)) {
       return state;
@@ -106,7 +83,6 @@ const rootReducer = (state = initState, action) => {
   }
   //A minus has been pressed
   if (action.type === 'DISPLAY_MINUS') {
-    console.log('A minus has been pressed');
     //if last entered is minus
     if (state.lastEntered === '-') {
       return state;
@@ -121,9 +97,10 @@ const rootReducer = (state = initState, action) => {
   }
   //An equal has been pressed
   if (action.type === 'DISPLAY_EQUAL') {
-    console.log('An equal has been pressed');
-    console.log('EQUAL: ', state.pastCalculation);
-    calculation(state.pastCalculation);
+    return {
+      ...state,
+      display: doCalculation(state.pastCalculation),
+    };
   }
   //A reset has been pressed
   if (action.type === 'CLEAR_ACTION') {
@@ -140,55 +117,55 @@ const rootReducer = (state = initState, action) => {
 //Return true if the display is already dec
 const isDec = (str) => str.indexOf('.') >= 0;
 
-const calculation = (arr) => {
-  //list of minus number
-  //list of operator
-  let a = cleanOperation(arr);
-  console.log(a);
-};
-const cleanOperation = (arr) => {
-  let nbr = [];
-  return [...arr].reduce((acc, val, i) => {
-    console.log('processing: ', val, 'acc', acc);
-    let length = arr.length;
-    //we start with a minus nbr
-    if (val === '-' && i === '0') {
-      nbr.push(val);
-      return acc;
+const doCalculation = (arr) => {
+  console.log('string', arr.join(''));
+  const regexNbr = /(\d+\.\d+)|([*\/]-\d+)|(\d+)/g;
+  const regexOpr = /([+\*\/-]{2})|([+\*\/-])/g;
+
+  const nbr = arr
+    .join('')
+    .match(regexNbr)
+    .map((val) => {
+      let fullNumber = [...val]
+        .filter((number) => {
+          return number !== '/' && number !== '*';
+        })
+        .join('');
+      console.log(val, '|', fullNumber);
+      if (fullNumber.indexOf('.') >= 0) {
+        return parseFloat(fullNumber);
+      }
+      return parseInt(fullNumber);
+    });
+  const opr = arr
+    .join('')
+    .match(regexOpr)
+    .map((val) => {
+      if (val.length !== 1) {
+        if (val[1] === '+') {
+          return val[1];
+        }
+        return val[0];
+      } else {
+        return val;
+      }
+    });
+  const calculation = [...nbr].reduce((acc, val, i) => {
+    switch (opr[i - 1]) {
+      case '+':
+        return acc + val;
+      case '-':
+        return acc - val;
+      case '*':
+        return acc * val;
+      case '/':
+        return acc / val;
+      default:
+        break;
     }
-    //if its a number we continu
-    if (!isNaN(val)) {
-      nbr.push(val);
-      return acc;
-    }
-    //if its an operator we push nbr into acc
-    if (isNaN(val) && val !== '-') {
-      console.log('pass:');
-      acc.push(nbr.join(''));
-      acc.push(val);
-      nbr = [];
-      return acc;
-    }
-    if (val === '-' && isNaN(arr[i - 1])) {
-      acc.push(nbr.join(''));
-      nbr = [];
-      nbr.push(val);
-      return acc;
-    }
-    if (val === '-' && isNaN(arr[i - 1])) {
-      acc.push(nbr.join(''));
-      nbr = [];
-      nbr.push(val);
-      return acc;
-    }
-    if (val === '-' && !isNaN(arr[i - 1])) {
-      acc.push(nbr.join(''));
-      nbr = [];
-      acc.push(val);
-      return acc;
-    }
-    return acc;
-    //s'il est précédé de rien ou d'un operateur c'est un nbr negatif
-  }, []);
+  });
+  console.log('nbr', nbr);
+  console.log('opr', opr);
+  return calculation;
 };
 export default rootReducer;
